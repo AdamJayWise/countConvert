48349c340bdeda0e4cd153c3b84c49fb148bdb84
AdamWise <wise.adam.jay@gmail.com>
2020-07-06 16:49:15 -0400

    added em gain

---
+++ js/cameraInfo.txt
@@ -0,0 +1,11 @@
+‘iXon’ : {
+	‘iXon Ultra 897’ :
+		{
+			‘offSet’ : 100,
+			‘readModes’ :
+			{
+				‘17MHz, preamp 1’ : 16.5,
+				‘17MHz, preamp 2’ : 
+}
+		}
+}
--- js/countConvert.js
+++ js/countConvert.js
@@ -23,14 +23,57 @@ var validRange = {
     'gain' : { 'min': 0, 'max' : 10**9},
     'qe' : { 'min': 0, 'max' : 1},
     'offset' : { 'min': 0, 'max' : 10**9},
+    'emGain' : { 'min': 1, 'max' : 1000},
     
 }
+
+
+// a function to append an inline div that pops up a new closeable div with a tooltip inside
+function addToolTip(targetSelection, message){
+    var tipButton = targetSelection.append('div');
+    tipButton.classed('tipButton', true);    
+    tipButton.text('?');
+
+    tipButton.on('click', function(){
+
+        var screenDiv = d3.select('body').append('div')
+        screenDiv.classed('screen', true);
+        screenDiv.on('click', function(){
+            screenDiv.remove();
+            messageDiv.remove();
+        })
+        
+        var messageDiv = d3.select('body').append('div').classed('messageDiv', true);
+        messageDiv.style('left', Math.max(10, d3.event.x - messageDiv.style('width').split('px')[0] )+ 'px');
+        messageDiv.style('top', d3.event.y + 'px')
+        messageDiv.text(message);
+        messageDiv.on('click', function(){
+            screenDiv.remove();
+            messageDiv.remove();
+        })
+        
+    })
+
+    return tipButton;
+
+}
+
+var tipDict = {
+    'gain':"Analog to Digital Converter (ADC) gain is the exchange rate between electrons in a pixel and the digital number representation of that pixel.  Here it's shown in counts per electron, or how many electrons must be in a pixel to register a single 'count' in the raw data set.",
+    'offset' : "An offset is applied to the signal during digitization to simplify digital bookkeeping.  It isn't created by light and needs to be removed to get an accurate count of photons, but luckily it will the same for each pixel in a single image.  Generally this offset is a few hundred counts, but varies camera-to-camera and on the specific acquisition mode used.",
+    'qe' : "Quantum Efficiency (QE) is the ratio of photons incident on a pixel to electrons collected from that pixel, before any EM gain is applied.  Here it is specified from 0 to 1, although percentages are common as well.  QE depends strongly on wavelength and sensor type.",
+}
+
+d3.selectAll('.eqInput').each(function(p,j){
+    var thisParam = this.id.split('Input')[0];
+    addToolTip(d3.select(this.parentNode.parentNode).select('.inputLabel'), tipDict[thisParam])
+})
 
 
 function updateVals(param){
 
     if (param == 'photons'){
-        var newCounts = (app['photons'] * app['qe']) * app['gain'] + app['offset'];
+        var newCounts = (app['photons'] * app['qe'] * app['emGain']) * app['gain'] + app['offset'];
         console.log(newPhotons, ' counts would be measured')
     
         if (!isNaN(newCounts)){
@@ -41,13 +84,14 @@ function updateVals(param){
             }
 
             d3.select('#countInput').property('value', r(newCounts,2))
+            d3.select('#messages').text('Enter Values to Update').style('color', 'black')
             app['counts'] = newCounts;
         }
         return
     }
 
     //d3.select('#')
-    var newPhotons = (app['counts'] - app['offset']) / app['gain'] / app['qe'];
+    var newPhotons = (app['counts'] - app['offset']) / app['gain'] / app['qe'] / app['emGain'];
     console.log(newPhotons, ' photons were detected')
 
     if (!isNaN(newPhotons)){
